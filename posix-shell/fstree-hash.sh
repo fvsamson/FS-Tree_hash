722d1146a1f9a4d30b94466386210c1c60ff5d47
Florian von Samson <167841080+fvsamson@users.noreply.github.com>
2024-07-13 20:38:02 +0200

    Update fstree-hash.sh

--- posix-shell/fstree-hash.sh
+++ posix-shell/fstree-hash.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -u  # Error on expanding unset parameters (e.g. variables); may careully consider -e (exit on errors);
+set -u  # Error on expanding unset parameters (e.g. variables); may carefully consider -e (exit on errors);
         # -f (noglob) would limit functionality, -C (noclobber) is superfluous
 export POSIXLY_CORRECT=1  # Enhances portability (at the expense of special functionality which is not wanted here)
 
@@ -9,16 +9,14 @@ fi
 
 hash_cmdln="sha256sum -b "  # The default, if no hash algorithm is set by option "-a" or "--alg(orithm)"
 hash_text=SHA2-256
+hash_broken=no
 
-while [ $# -gt 0 ]
+while [ $# -gt 1 ]
 do
   
-# First parameter must be a valid path as the root of the tree to be hashed
-if ! [ -e "$1" ]
-then exit 1
-fi
 
 case "$1" in
+  
 -a*|--alg*)
   hash_param="$2"
   # Second option parameter specifies a hash algorithm.
@@ -28,7 +26,7 @@ case "$1" in
     hash-alg=sha224
     shift
     ;;
-  sha256|sha-256|sha2-256|SHA256|SHA-256|SHA2-256|"")
+  sha256|sha-256|sha2-256|SHA256|SHA-256|SHA2-256)
     hash-alg=sha256
     shift
     ;;
@@ -44,11 +42,22 @@ case "$1" in
     hash-alg=b2
     shift
     ;;
-  sha1|sha-1|SHA1|SHA-1|md5|MD5)
-    echo "" 2>
-    exit 3
+  sha1|sha-1|SHA1|SHA-1)
+    hash_broken=yes
+    
+    ;;
+  md5|MD5)
+    hash_broken=yes
+    
     ;;
   *)
     exit 2
     ;;
   esac
+  echo "Mind that $hash_text is cryptographically broken and hence dangerous and discouraged." 2>
+done
+
+# The only non-option, hence last parameter must be a valid path as the root of the tree to be hashed
+if ! [ -e "$1" ]
+then exit 1
+fi
